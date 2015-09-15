33c16d2a79e55f8343d6c695637852402ca45f29
Jenny <jennifer.voney@students.fhnw.ch>
2015-09-15 14:45:03 +0200

    test

---
+++ PersonAdmin/PersonAdminApplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using PersonAdminLib;
+
+namespace PersonAdmin
+{
+    class PersonAdminApplication
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person("Hans", "Muster");
+            Console.WriteLine("My first c# Program: {0}",
+                Assembly.GetExecutingAssembly().GetName().Version);
+            Console.WriteLine("{0} {1}", firstPerson.GetFirstName(), firstPerson.GetSurName());
+            Console.ReadKey();
+        }
+    }
+}
